7bd1dd823ab35dcc1295cd09d21a4f9d2029a6dd
nityaaajain <jenith1975@gmail.com>
2025-10-08 14:58:55 +0530

    Lec21

---
+++ Week06/freq.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <string.h>
+int main(){
+    char str[]="abshs";
+    int freq[256]={0};
+    int len= strlen(str);
+    for(int i=0;i<len;i++){
+        freq[str[i]]++;
+
+    }
+    for(int i=0;i<256;i++){
+        if(freq[i]>0)
+        printf("%c:%d\n",i,freq[i]);
+    }
+    
+}
